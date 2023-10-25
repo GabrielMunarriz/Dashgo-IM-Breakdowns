46d8ce8ba4ccd9b72753eb6e312b373efd9a24b1
gabmunarriz <gabmunarriz@gmail.com>
2023-10-25 13:55:06 +0800

    Removed hardcode for artist name. updated variable names

--- DashGoProject.py
+++ DashGoProject.py
@@ -8,17 +8,19 @@ continueLoop = True
 while continueLoop:
     
     #---User Input---
-    DistributionService = input("Choose Distribution Service (DashGo or IdentityMusic): ")
-    Month = input("Choose Month (MM): ")
-    Year = input("Choose Year (YY): ")
+    distributionService = input("Choose Distribution Service (DashGo or IdentityMusic): ")
+    artistName = input("Specify Artist Name: ")
+    month = input("Choose Month (MM): ")
+    year = input("Choose Year (YY): ")
 
 
-    if DistributionService.upper() == "DG" or "DASHGO":
+    if distributionService.upper() == "DG" or "DASHGO":
 
         #---Create Filename String---
-        DashGoCSV_filename = "landq-"
-        DashGoCSV_filename = DashGoCSV_filename + Month + "-"
-        DashGoCSV_filename = DashGoCSV_filename + Year + ".csv"
+        #Format: artistname-MM-YY.csv (e.g. landq-09-23.csv)
+        DashGoCSV_filename = artistName + "-"
+        DashGoCSV_filename = DashGoCSV_filename + month + "-"
+        DashGoCSV_filename = DashGoCSV_filename + year + ".csv"
 
         #---Read Filename---
         DashGoCSV = pd.read_csv(DashGoCSV_filename)
@@ -45,7 +47,7 @@ while continueLoop:
         print("Summary")
         print(RAPB_Stats)
 
-    elif DistributionService.upper() == "IM" or "IDENTITYMUSIC":
+    elif distributionService.upper() == "IM" or "IDENTITYMUSIC":
         print("No IdentityMusic Functionality yet")
     
     print(" ") #Line Break
