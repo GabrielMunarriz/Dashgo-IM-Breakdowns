d39c493061511d6aebed9d02d90895a511611202
gabmunarriz <gabmunarriz@gmail.com>
2023-10-25 14:59:30 +0800

    Added pie chart operation. Added as_index=False parameter on RAPB groupby function

--- DashGoProject.py
+++ DashGoProject.py
@@ -39,34 +39,53 @@ while continueLoop:
         DashGoCSV_filename = DashGoCSV_filename + year + ".csv"
 
         #---Read Filename---
-        DashGoCSV = pd.read_csv(DashGoCSV_filename)
+        DashGoCSV_DF = pd.read_csv(DashGoCSV_filename)
 
-        #---Formatting DashGo CSV (Generalized)---
-        #Remove Duplicate Rows from Self Releases
-        DashGoCSV.drop(DashGoCSV[DashGoCSV['Deal Share'] == "0.0%"].index, inplace=True) #I removed these rows because they're duplicates from tracks that I released via self-release. 
-                                                                                         #When self-releasing, rows are duplicated because I am both the label and the artist and a row is created for each "side" of the release.
-                                                                                         #a row X with 0.0% Deal Share is created on the label's side while an identical row X with 100% deal share is created on the artist's side
-                                                                                         #It is this way because I made 100% of royalties go to the artist (Me) for self-releases.
+        #---Formatting DashGo CSV---
+        
+        # Formatting -- General
+        
+        # Remove Duplicate Rows from Self Releases
+        DashGoCSV_DF.drop(DashGoCSV_DF[DashGoCSV_DF['Deal Share'] == "0.0%"].index, inplace=True) #I removed these rows because they're duplicates from tracks that I released via self-release. 
+                                                                                                  #When self-releasing, rows are duplicated because I am both the label and the artist and a row is created for each "side" of the release.
+                                                                                                  #a row X with 0.0% Deal Share is created on the label's side while an identical row X with 100% deal share is created on the artist's side
+                                                                                                  #It is this way because I made 100% of royalties go to the artist (Me) for self-releases.
+        
+        
+        # Formatting -- Revenue and Payable Breakdown
+        
+        # Detailed Breakdown
+        RAPB = DashGoCSV_DF.groupby(['Track Title', 'Label Name','Distribution Rate','Deal Share'], as_index=False)[['Net Revenue','Payable']].sum() #Formatting
+        RAPB = RAPB.sort_values(by = ['Payable'], ascending=[False]) #Sort
+        
+        # Summarized Breakdown
+        RAPB_Stats = RAPB.sum(numeric_only=True)
         
         if operation == "1":
         
-            #---Revenue and Payable Breakdown---
-            RAPB = DashGoCSV.groupby(['Track Title', 'Label Name','Distribution Rate','Deal Share'])[['Net Revenue','Payable']].sum() #Formatting
-            RAPB = RAPB.sort_values(by = ['Payable'], ascending=[False]) #Sort
+            #---Printing Revenue and Payable Detailed Breakdown---
             #RAPB
             print("Breakdown")
             print(RAPB)
-
             print(" ") #Line Break
 
-            #---Revenue and Payable Summary---
-            RAPB_Stats = RAPB.sum()
+            #---Printing Revenue and Payable Summarized Breakdown---
+            
             #RAPB_Stats
             print("Summary")
             print(RAPB_Stats)
+            print(" ") #Line Break
             
         elif operation == "2":
-            print ("no operation 2 yet.")
+            #print ("no operation 2 yet.")
+            
+            print("")
+            RAPB_Payable = RAPB['Payable']
+            RAPB_Payable_Labels = RAPB['Track Title']
+            
+            plt.pie(RAPB_Payable, labels = RAPB_Payable_Labels)
+            plt.show()
+            
 
     #--IdentityMusic--
     elif distributionService == "2":
